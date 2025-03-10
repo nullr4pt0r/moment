d2f4ba7a3d47d5aa5d325b9bdfcc6632aa8c9d9a
nullr4pt0r <67836886+nullr4pt0r@users.noreply.github.com>
2025-03-11 00:43:34 +0530

    Update CorsConfig.java

--- src/main/java/io/nullr4pt0r/moment/config/CorsConfig.java
+++ src/main/java/io/nullr4pt0r/moment/config/CorsConfig.java
@@ -9,16 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Adjust the mapping as needed
-                        .allowedOrigins("https://moments-word-rain.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://moments-word-rain.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 }
