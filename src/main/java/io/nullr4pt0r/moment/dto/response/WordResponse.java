e2ec3cd836a41ba41e988e9ffa7951508a590e12
nullr4pt0r <67836886+nullr4pt0r@users.noreply.github.com>
2025-03-12 00:20:03 +0530

    Update WordResponse.java (#5)

--- src/main/java/io/nullr4pt0r/moment/dto/response/WordResponse.java
+++ src/main/java/io/nullr4pt0r/moment/dto/response/WordResponse.java
@@ -2,5 +2,5 @@ package io.nullr4pt0r.moment.dto.response;
 
 import java.util.List;
 
-public record WordResponse(String word, String language, List<String> meanings, String englishTranslation, List<String> remarks) {
+public record WordResponse(String word, String language, List<String> meanings, String phonetics, String englishTranslation, List<String> remarks) {
 }
